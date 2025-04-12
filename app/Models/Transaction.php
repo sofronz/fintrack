6d547c434e0983e2573fbaa968bf43add958ca1c
Sofronius Ruddy <ruddy@circlecreative.dev>
2025-04-12 23:34:52 +0700

    docs(model): add doc

--- app/Models/Transaction.php
+++ app/Models/Transaction.php
@@ -4,6 +4,31 @@ namespace App\Models;
 use App\Enum\TransactionType;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $code
+ * @property string $transaction_date
+ * @property TransactionType $transaction_type
+ * @property string $amount
+ * @property int $category_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Category $category
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereCategoryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereTransactionDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereTransactionType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Transaction whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Transaction extends Model
 {
     /**
